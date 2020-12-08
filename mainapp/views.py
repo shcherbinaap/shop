ef06a0699e3a492a3e3e16f710657f678b7c6ad7
shcherbinaap <sherbus@mail.ru>
2020-12-08 19:54:40 +0300

    Файлы урока 1

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 def main(request):
     return render(request, 'mainapp/index.html')
 
-
-
 def products(request):
     return render(request, 'mainapp/products.html')
+
+def text_context(request):
+    return render(request, 'maimapp/context.html')
