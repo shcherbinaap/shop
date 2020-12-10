ea4b51953e2a2ace7d1af7dd03d7258187f54db8
shcherbinaap <sherbus@mail.ru>
2020-12-11 00:05:38 +0300

    Файлы урока 3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,65 +1,71 @@
 from django.shortcuts import render
-
+from mainapp.models import Product, ProductCategory
 
 def main(request):
     context = {
         'titlepage': 'главная',
 
     }
-    return render(request, 'mainapp/index.html', context)
+    return render(request, 'mainapp/index.html', context=context)
 
 def products(request):
-    context = {
+    # context = {
+    #     'titlepage': 'каталог товаров',
+    #     'products': [
+    #         {
+    #             'name': 'Худи черного цвета с монограммами adidas Originals',
+    #             'price': '6 090,00 руб.',
+    #             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+    #             'description_shot': '',
+    #             'img': 'vendor/img/products/Adidas-hoodie.png',
+    #         },
+    #         {
+    #             'name': 'Синяя куртка The North Face',
+    #             'price': '23 725,00 руб.',
+    #             'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+    #             'description_shot': '',
+    #             'img': 'vendor/img/products/Blue-jacket-The-North-Face.png',
+    #         },
+    #         {
+    #             'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+    #             'price': '3 390,00 руб.',
+    #             'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+    #             'description_shot': '',
+    #             'img': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
+    #         },
+    #         {
+    #             'name': 'Черный рюкзак Nike Heritage',
+    #             'price': '2 340,00 руб.',
+    #             'description': 'Плотная ткань. Легкий материал.',
+    #             'description_shot': '',
+    #             'img': 'vendor/img/products/Black-Nike-Heritage-backpack.png',
+    #         },
+    #         {
+    #             'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
+    #             'price': '13 590,00 руб.',
+    #             'description': 'Гладкий кожаный верх. Натуральный материал.',
+    #             'description_shot': '',
+    #             'img': 'vendor/img/products/Black-Dr-Martens-shoes.png',
+    #         },
+    #         {
+    #             'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
+    #             'price': '2 890,00 руб.',
+    #             'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
+    #             'description_shot': '',
+    #             'img': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
+    #         },
+    #
+    #     ],
+    #
+    # }
+
+    contex = {
         'titlepage': 'каталог товаров',
-        'products': [
-            {
-                'name': 'Худи черного цвета с монограммами adidas Originals',
-                'price': '6 090,00 руб.',
-                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-                'description_shot': '',
-                'img': 'vendor/img/products/Adidas-hoodie.png',
-            },
-            {
-                'name': 'Синяя куртка The North Face',
-                'price': '23 725,00 руб.',
-                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-                'description_shot': '',
-                'img': 'vendor/img/products/Blue-jacket-The-North-Face.png',
-            },
-            {
-                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
-                'price': '3 390,00 руб.',
-                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-                'description_shot': '',
-                'img': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png',
-            },
-            {
-                'name': 'Черный рюкзак Nike Heritage',
-                'price': '2 340,00 руб.',
-                'description': 'Плотная ткань. Легкий материал.',
-                'description_shot': '',
-                'img': 'vendor/img/products/Black-Nike-Heritage-backpack.png',
-            },
-            {
-                'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-                'price': '13 590,00 руб.',
-                'description': 'Гладкий кожаный верх. Натуральный материал.',
-                'description_shot': '',
-                'img': 'vendor/img/products/Black-Dr-Martens-shoes.png',
-            },
-            {
-                'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-                'price': '2 890,00 руб.',
-                'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-                'description_shot': '',
-                'img': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png',
-            },
-
-        ],
-
+        'products': Product.objects.all(),
+        'catalogs': ProductCategory.objects.all(),
     }
-    return render(request, 'mainapp/products.html', context)
 
+    return render(request, 'mainapp/products.html', context=contex)
 
 
 
--- shop/urls.py
+++ shop/urls.py
@@ -1,27 +1,16 @@
-"""shop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
-
 from mainapp import views as mainapp_views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp_views.main, name='index'),
-    path('products/', mainapp_views.products, name='products'),
-    path('text_context/', mainapp_views.text_context),
+    path('', mainapp_views.main, name='main'),
+    path('products/', include('mainapp.urls', namespace='products')),
+    # path('products/', mainapp_views.products, name='products'),
+    path('text_context/1/', mainapp_views.text_context),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
