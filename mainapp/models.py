7fe964ee3106ca00ea6d05baaade8ee9a9997083
shcherbinaap <sherbus@mail.ru>
2020-12-12 19:06:21 +0300

    Файлы урока 3

--- mainapp/models.py
+++ mainapp/models.py
@@ -8,7 +8,6 @@ class ProductCategory(models.Model):
 	def __str__(self):
 		return self.name
 
-
 class Product(models.Model):
 	name = models.CharField(max_length=256)
 	image = models.ImageField(upload_to='products_images', blank=True)
@@ -20,5 +19,3 @@ class Product(models.Model):
 
 	def __str__(self):
 		return f'{self.name} {self.category.name}'
-
-
