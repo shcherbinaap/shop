3aa776b949489e325d9b06f8342bc5d49ec4e816
shcherbinaap <sherbus@mail.ru>
2020-12-17 21:41:53 +0300

    Файлы урока 5. Добавлена обработка ошибок

--- authapp/views.py
+++ authapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
 from authapp.forms import UserLoginForm, UserRegisterForm
 
@@ -32,6 +32,7 @@ def register(request):
         form = UserRegisterForm(data = request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегистрировались!')
             return HttpResponseRedirect(reverse('authapp:login'))
 
     else:
