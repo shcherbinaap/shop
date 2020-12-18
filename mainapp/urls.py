4cb49e8aec4de113da51550d04d8fb2a614e86a1
shcherbinaap <sherbus@mail.ru>
2020-12-18 19:05:09 +0300

    Файлы урока 4. Скорректировал URL mainapp

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,5 @@ app_name = 'mainapp'
 
 urlpatterns = [
 	path('', mainapp.products, name='products'),
-	path('<int:pk>/', mainapp.products, name='product'),
+	path('product/<int:pk>/', mainapp.products, name='product'),
 ]
