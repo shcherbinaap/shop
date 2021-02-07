1909d3887625b6183d7dd923b383f5ed70ef3919
shcherbinaap <sherbus@mail.ru>
2021-02-07 11:47:23 +0300

    Django Framework. Инструменты оптимизации Файлы урока 2-5. Работа с сервером. На этом этапе на сервере все работает

--- mainapp/management/commands/update_db.py
+++ mainapp/management/commands/update_db.py
@@ -9,6 +9,8 @@
 #         for user in users:
 #             UserProfile.objects.create(user = user)
 
+# TODO разобраться с работой файла
+
 from django.core.management.base import BaseCommand
 from mainapp.models import ProductCategory, Product
 from django.contrib.auth.models import User
@@ -28,19 +30,22 @@ class Command(BaseCommand):
 
         categories = load_from_json('categories')
 
-        ProductCategory.objects.all().delete()
-        for category in categories:
-            new_category = ProductCategory(**category)
-            new_category.save()
-        products = load_from_json('products')
-        Product.objects.all().delete()
-        for product in products:
-            category_name = product["category"]
-            # Получаем категорию по имени
-            _category = ProductCategory.objects.get(name = category_name)
-            # Заменяем название категории объектом
-            product['category'] = _category
-            new_product = Product(**product)
-            new_product.save()
-        # Создаем суперпользователя при помощи менеджера модели
-        super_user = User.objects.create_superuser('django', 'django@shop.local', 'geekbrains')
+        # ProductCategory.objects.all().delete()
+        # for category in categories:
+            # print(**category['fields'])
+            # new_category = ProductCategory(**category)
+        #     new_category = ProductCategory(category['fields'])
+        #
+            # new_category.save()
+        # products = load_from_json('products')
+        # Product.objects.all().delete()
+        # for product in products:
+        #     category_name = product["category"]
+        #     # Получаем категорию по имени
+        #     _category = ProductCategory.objects.get(name = category_name)
+        #     # Заменяем название категории объектом
+        #     product['category'] = _category
+        #     new_product = Product(**product)
+        #     new_product.save()
+        # # Создаем суперпользователя при помощи менеджера модели
+        # super_user = User.objects.create_superuser('django', 'django@shop.local', 'geekbrains')
